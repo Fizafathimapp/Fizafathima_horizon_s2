434c16528c8176aaec2e904b4ae07940ea9c20ee
Fiza Fathima PP <fizappm0032@gmail.com>
2026-03-18 08:22:44 +0530

    added coments

---
+++ README.txt
@@ -0,0 +1,39 @@
+Rover Motion Simulation Program
+
+Description:
+This program calculates the distance and time required for a rover to move 
+from an origin point to a destination point based on user input.
+
+Requirements:
+- Python 3.x installed
+
+How to Run:
+
+1. Open Command Prompt
+2. Navigate to the folder containing the file:
+   cd Desktop   (or your file location)
+
+3. Run the program:
+   python rover.py
+
+Inputs Required:
+- Origin coordinates (x1, y1)
+- Destination coordinates (x2, y2)
+- Initial velocity (m/s)
+- Acceleration (m/s^2)
+- Maximum velocity (m/s)
+
+Output:
+- Distance between points
+- Time required to reach destination
+
+Example:
+Enter origin x1: 0
+Enter origin y1: 0
+Enter destination x2: 100
+Enter destination y2: 40
+Enter initial velocity: 2
+Enter acceleration: 1
+Enter maximum velocity: 10
+
+by: FIZA FATHIMA PP
--- rover.py
+++ rover.py
@@ -1,16 +1,23 @@
-import math
+import math #used for square root calculation
 
 # -------- Part A: Distance Calculation --------
 try:
+    #taking inputs for orgin coordinates
     x1 = float(input("Enter origin x1: "))
     y1 = float(input("Enter origin y1: "))
+
+     #taking inputs for destination coordinates
     x2 = float(input("Enter destination x2: "))
     y2 = float(input("Enter destination y2: "))
 
+    #distance formula calculation
     distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+    #display distance
     print(f"Distance to destination: {distance:.2f} meters")
 
 # -------- Part B: Motion Parameters --------
+    #taking motion inputs
     u = float(input("Enter initial velocity (m/s): "))
     a = float(input("Enter acceleration (m/s^2): "))
     vmax = float(input("Enter maximum speed (m/s): "))
@@ -18,9 +25,9 @@ try:
     # Time to reach max speed
     time_to_vmax = (vmax - u) / a if a != 0 else 0
     distance_accel = (u * time_to_vmax) + (0.5 * a * time_to_vmax**2)
-
+    
+    # casse1 : Rover reaches destination before max speed
     if distance_accel >= distance:
-        # Rover reaches destination before max speed
         # Using: s = ut + 0.5at^2
         A = 0.5 * a
         B = u
@@ -30,11 +37,12 @@ try:
         time = (-B + math.sqrt(discriminant)) / (2*A)
 
     else:
-        # Rover reaches max speed then continues
+        # acse2 : Rover reaches max speed then continues
         remaining_distance = distance - distance_accel
         time_constant = remaining_distance / vmax
         time = time_to_vmax + time_constant
-
+    
+    #display total time required
     print(f"Time required: {time:.2f} seconds")
 
 # -------- Part C: Error Handling --------
